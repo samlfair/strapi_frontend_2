cf9a000fdcb50b49012150df7556dbef8baa3f4d
Samuel Littlefair-Wallace <sam@littlefairwallace.ca>
2020-06-17 22:31:33 +0200

    Updates

---
+++ precache-manifest.31774de50f75adea47e7999cca5b19cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca9a525a9b612270575cd40090d6822",
+    "url": "/strapi_frontend_2/index.html"
+  },
+  {
+    "revision": "847de4fd9164b6020742",
+    "url": "/strapi_frontend_2/static/css/main.f3015d9c.chunk.css"
+  },
+  {
+    "revision": "a61d31a10bf8baff4c11",
+    "url": "/strapi_frontend_2/static/js/2.9f5c13ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/strapi_frontend_2/static/js/2.9f5c13ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "847de4fd9164b6020742",
+    "url": "/strapi_frontend_2/static/js/main.d0d74e18.chunk.js"
+  },
+  {
+    "revision": "9d4756cbd947719cffea",
+    "url": "/strapi_frontend_2/static/js/runtime-main.89d0af14.js"
+  }
+]);
